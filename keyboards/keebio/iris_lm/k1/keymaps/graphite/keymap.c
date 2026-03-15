cb62801fa1713c5bd548fa008c32feb0f3a8c541
Yuval.D <yuvddd@pm.me>
2026-03-15 18:34:27 +0200

    updated iris and simpifield root scripts

--- keyboards/keebio/iris_lm/k1/keymaps/graphite/keymap.c
+++ keyboards/keebio/iris_lm/k1/keymaps/graphite/keymap.c
@@ -88,8 +88,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // ── Base (Graphite) ─────────────────────────────────────────────
     [Base] = LAYOUT(
         KC_F1,          KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,             KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
-        KC_NO,          LT(Sys,KC_B), KC_L, KC_D,  KC_W,    KC_Z,             KC_SCLN, KC_F,    KC_O,    KC_U,    KC_J,    KC_RSFT,
-        KC_LGUI,        KC_N,    KC_R,    KC_T,    KC_S,    KC_G,              KC_Y,    KC_H,    KC_A,    KC_E,    KC_I,    MO(Num),
+        MO(Num),        LT(Sys,KC_B), KC_L, KC_D,  KC_W,    KC_Z,             KC_SCLN, KC_F,    KC_O,    KC_U,    KC_J,    KC_RSFT,
+        KC_LGUI,        KC_N,    KC_R,    KC_T,    KC_S,    KC_G,              KC_Y,    KC_H,    KC_A,    KC_E,    KC_I,    KC_NO,
         TD_LSFT,        KC_Q,    KC_X,    KC_M,    KC_C,    KC_V,    KC_VOLD,  KC_VOLU, KC_K,    KC_P,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
                                           LCTL_T(KC_ESC), MO(Sym),KC_SPC,    KC_ENT,  KC_BSPC, KC_LALT
     ),
@@ -107,9 +107,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // ── Numbers ─────────────────────────────────────────────────────
     [Num] = LAYOUT(
         KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                    KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
-        _______, _______, KC_7,    KC_8,    KC_9,    _______,                  MM_STAR, KC_7,    KC_8,    KC_9,    KC_MINS, _______,
-        _______, _______, KC_4,    KC_5,    KC_6,    _______,                  KC_EQL,  KC_4,    KC_5,    KC_6,    KC_PLUS, _______,
-        KC_TAB,  KC_0,    KC_1,    KC_2,    KC_3,    _______, _______, _______,  MM_SLASH,KC_1,    KC_2,    MM_N3,   KC_0,    _______,
+        _______, KC_HOME, KC_PGUP, KC_PGDN, KC_END,  _______,                  MM_STAR, KC_7,    KC_8,    KC_9,    KC_MINS, _______,
+        _______, KC_LEFT, KC_UP,   KC_DOWN, KC_RGHT, _______,                  KC_EQL,  KC_4,    KC_5,    KC_6,    KC_PLUS, _______,
+        KC_TAB,  _______, _______, _______, _______, _______, _______, _______,  MM_SLASH,KC_1,    KC_2,    MM_N3,   KC_0,    _______,
                                    _______, _______, _______,                  _______, _______, _______
     ),
 
